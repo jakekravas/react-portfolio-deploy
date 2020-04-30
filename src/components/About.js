47c6cc307730abc9c3770649d1b836e95005eac2
Jake Kravas <jakekravas7@gmail.com>
2020-04-29 23:09:26 -0700

    updated resume link

--- src/components/About.js
+++ src/components/About.js
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
-    return (
-        <section id="about-section">
-            <div className="dark-overlay">
-                <div className="about-inner container">
-                    <div className="row">
-                        <div className="col text-white">
-                            <h1 className="display-4">Aspiring <strong>Full-Stack</strong> Web Developer</h1>
-                            <div className="p4 my-4">Recent graduate of the Full-Stack Web Development Bootcamp through The University of Arizona. With skills in JavaScript, React, Node.js, MongoDB, MySQL, HTML, CSS, and various other technologies, I'd be eager to take a position as a front-end, back-end, or full-stack web developer.</div>
-                            <a href="https://drive.google.com/file/d/1InyNh6xqkZnmmhqiFqh2fVtfK2WV0aSq/view" target="_blank" className="btn btn-primary btn-lg mr-2 ss">View Resume</a>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <section id="about-section">
+      <div className="dark-overlay">
+        <div className="about-inner container">
+          <div className="row">
+            <div className="col text-white">
+              <h1 className="display-4">Aspiring <strong>Full-Stack</strong> Web Developer</h1>
+              <div className="p4 my-4">Recent graduate of the Full-Stack Web Development Bootcamp through The University of Arizona. With skills in JavaScript, React, Node.js, MongoDB, MySQL, HTML, CSS, and various other technologies, I'd be eager to take a position as a front-end, back-end, or full-stack web developer.</div>
+              <a href="https://drive.google.com/file/d/18aYq5meM-k9zd2mJh8bRZqH34-bKrWFe/view?usp=sharing" rel="noopener noreferrer" target="_blank" className="btn btn-primary btn-lg mr-2 ss">View Resume</a>
             </div>
-        </section>
-    )
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default About;
